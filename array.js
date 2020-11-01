545008394bf93206ca4f8f159997ce35f5ccd7eb
Apramey Bhat <aprameya.rb@gmail.com>
2020-11-01 18:19:42 +0530

    Added array.js

---
+++ array.js
@@ -0,0 +1,58 @@
+//classes
+class car{
+    constructor(name, year){
+        this.name = name;
+        this.year= year;
+    }
+
+    age(){
+        let presentDate = new Date();
+        return presentDate.getFullYear() - this.year;
+    }
+}
+
+let aCar = new car('sx4', 2010);
+console.log(aCar.age());
+
+//arrays 
+class array{
+    constructor(){
+        this.length = 0;
+        this.data = {};
+    }
+
+    get(){
+        return this.data;
+    }
+    length(){
+        return this.length;
+    }
+    push(item){
+        this.data[this.length] = item;
+        this.length++;
+        return this.length;
+    }
+    pop(){
+        delete this.data[this.length - 1];
+        this.length--;
+    }
+    delete(index){
+        this.shiftItems(index);
+    }
+    shiftItems(index){
+        for(let i=index; i< this.length-1; i++){
+            this.data[i] = this.data[i+1];            
+        }
+        delete this.data[this.length-1];
+        this.length--;
+    }
+}
+
+let aArray = new array();
+aArray.push(1);
+aArray.push(2);
+aArray.push('hi');
+aArray.pop();
+aArray.push('hello');
+aArray.delete(0);
+console.log(aArray.data);
