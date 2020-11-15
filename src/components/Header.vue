5564f4608e9e01474c0d10008c3f7e5e5393efd5
Apramey Bhat <aprameya.rb@gmail.com>
2020-11-15 16:49:11 +0530

    update project

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="header">
+    <img alt="dark mode" src="../assets/icons/dark_mode.png">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props : {
+      msg : String
+  }
+}
+</script>
